1cc84ae9f38b50757fd41e65b0d81a2e9baaef51
Tieenss <tiengame2k5@gmail.com>
2026-01-07 15:51:29 +0700

    Da Update

--- QuanLyHocSinh/QuanLyHocSinh/Program.cs
+++ QuanLyHocSinh/QuanLyHocSinh/Program.cs
@@ -17,7 +17,7 @@ namespace QuanLyHocSinh
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmQuanLyHanhKiem());
+            Application.Run(new FrmLichThi());
         }
     }
 }
