364459adf2d8d5e68fee84468e27365bb154b9f8
Tieenss <tiengame2k5@gmail.com>
2026-01-07 14:55:50 +0700

    Dọn dẹp thư mục rác bin và obj

--- QuanLyHocSinh/QuanLyHocSinh/Database/DatabaseHelper.cs
+++ QuanLyHocSinh/QuanLyHocSinh/Database/DatabaseHelper.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Data;
-using System.Data.SqlClient; // Nếu báo lỗi dòng này thì cài NuGet System.Data.SqlClient
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace QuanLyHocSinh.Database
 {
     public class DatabaseHelper
     {
-        // CẤU HÌNH KẾT NỐI (Dành cho bản SQL Server Express)
-        // Lưu ý: Thay '123456' bằng mật khẩu tài khoản sa của bạn
-        private static string strCon = @"Data Source=.\SQLEXPRESS;Initial Catalog=QuanLyHocSinh;User ID=sa;Password=123456";
+        // CHUỖI KẾT NỐI ĐẾN SMARTERASP.NET
+        // Lưu ý: Thay thế các thông tin bên dưới bằng thông tin thực tế từ Control Panel của bạn
+        private static string strCon = @"Server=SQL1003.site4now.net;Database=db_ac36c5_quanlyhocsinh;User Id=db_ac36c5_quanlyhocsinh_admin;Password=tien@362005;TrustServerCertificate=True;";
 
         // Hàm lấy dữ liệu (SELECT)
         public static DataTable GetData(string query)
@@ -28,7 +28,8 @@ namespace QuanLyHocSinh.Database
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Lỗi kết nối CSDL: " + ex.Message);
+                // In ra chi tiết lỗi để dễ debug
+                MessageBox.Show("Lỗi kết nối CSDL Online: " + ex.Message, "Thông báo lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             return dt;
         }
@@ -49,7 +50,7 @@ namespace QuanLyHocSinh.Database
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Lỗi thực thi lệnh: " + ex.Message);
+                MessageBox.Show("Lỗi thực thi lệnh Online: " + ex.Message, "Thông báo lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
--- QuanLyHocSinh/QuanLyHocSinh/Program.cs
+++ QuanLyHocSinh/QuanLyHocSinh/Program.cs
@@ -17,7 +17,7 @@ namespace QuanLyHocSinh
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmLichThi());
+            Application.Run(new FrmQuanLyDiem());
         }
     }
 }
